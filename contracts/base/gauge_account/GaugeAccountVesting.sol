85ea9ca26cdd5f422fb6f3d20074584f91394a3d
Maga <steam.30mb1@gmail.com>
2022-05-24 23:50:24 +0300

    Refactored vesting

--- contracts/base/gauge_account/GaugeAccountVesting.sol
+++ contracts/base/gauge_account/GaugeAccountVesting.sol
@@ -6,139 +6,108 @@ import "./GaugeAccountStorage.sol";
 
 
 abstract contract GaugeAccountVesting is GaugeAccountStorage {
-//    function _isEven(uint64 num) internal pure returns (bool) {
-//        return (num / 2) == 0 ? true : false;
-//    }
-//
-//    function _rangeSum(uint64 range) internal pure returns (uint64) {
-//        if (_isEven(range)) {
-//            return (range / 2) * range + (range / 2);
-//        }
-//        return ((range / 2) + 1) * range;
-//    }
-//
-//    // interval should be less than max
-//    function _rangeIntervalAverage(uint32 interval, uint32 max) internal pure returns (uint256) {
-//        return (_rangeSum(uint64(interval)) * SCALING_FACTOR) / max;
-//    }
-//
-//    // only applied if _interval is bigger than vestingPeriod, will throw integer overflow otherwise
-//    function _computeVestedForInterval(uint128 _entitled, uint32 _interval) internal view returns (uint128, uint128) {
-//        uint32 periods_passed = ((_interval / vestingPeriod) - 1);
-//        uint32 full_vested_part = periods_passed * vestingPeriod + _interval % vestingPeriod;
-//        uint32 partly_vested_part = _interval - full_vested_part;
-//
-//        // get part of entitled reward that already vested, because their vesting period is passed
-//        uint128 clear_part_1 = uint128((((full_vested_part * SCALING_FACTOR) / _interval) * _entitled) / SCALING_FACTOR);
-//        uint128 vested_part = _entitled - clear_part_1;
-//
-//        // now calculate vested share of remaining part
-//        uint256 clear_part_2_share = _rangeIntervalAverage(partly_vested_part, vestingPeriod) / partly_vested_part;
-//        uint128 clear_part_2 = uint128(vested_part * clear_part_2_share / SCALING_FACTOR);
-//        uint128 remaining_entitled = vested_part - clear_part_2;
-//
-//        return (clear_part_1 + clear_part_2, remaining_entitled);
-//    }
-//
-//    // this is used only when lastRewardTime < farmEndTime, because newly entitled reward not emitted otherwise
-//    // will throw with integer overflow otherwise
-//    function _computeVestedForNewlyEntitled(uint128 _entitled, uint32 _poolLastRewardTime, uint32 _farmEndTime) internal view returns (uint128 _vested) {
-//        if (_entitled == 0) {
-//            return 0;
-//        }
-//        if (_farmEndTime == 0 || _poolLastRewardTime < _farmEndTime) {
-//            uint32 age = _poolLastRewardTime - lastRewardTime;
-//
-//            if (age > vestingPeriod) {
-//                (uint128 _vested_part, uint128 _) = _computeVestedForInterval(_entitled, age);
-//                return _vested_part;
-//            } else {
-//                uint256 clear_part_share = _rangeIntervalAverage(age, vestingPeriod) / age;
-//                return uint128(_entitled * clear_part_share / SCALING_FACTOR);
-//            }
-//        } else {
-//            uint32 age_before = _farmEndTime - lastRewardTime;
-//            uint32 age_after = math.min(_poolLastRewardTime - _farmEndTime, vestingPeriod);
-//
-//            uint128 _vested_before;
-//            uint128 _entitled_before;
-//            if (age_before > vestingPeriod) {
-//                (_vested_before, _entitled_before) = _computeVestedForInterval(_entitled, age_before);
-//            } else {
-//                uint256 clear_part_share = _rangeIntervalAverage(age_before, vestingPeriod) / age_before;
-//                _vested_before = uint128(_entitled * clear_part_share / SCALING_FACTOR);
-//                _entitled_before = _entitled - _vested_before;
-//            }
-//
-//            uint128 _vested_after = _entitled_before * age_after / vestingPeriod;
-//            return (_vested_before + _vested_after);
-//        }
-//    }
-//
-//    function _computeVesting(
-//        uint128 _amount,
-//        uint128[] _rewardDebt,
-//        uint256[] _accRewardPerShare,
-//        uint32 _poolLastRewardTime,
-//        uint32 _farmEndTime
-//    ) internal view returns (uint128[], uint128[], uint32[]) {
-//        uint32[] new_vesting_time = new uint32[](vestingTime.length);
-//        uint128[] newly_vested = new uint128[](_rewardDebt.length);
-//        uint128[] updated_entitled = new uint128[](_rewardDebt.length);
-//        uint128[] new_entitled = new uint128[](_rewardDebt.length);
-//
-//        for (uint i = 0; i < _rewardDebt.length; i++) {
-//            uint256 _reward = uint256(_amount) * _accRewardPerShare[i];
-//            new_entitled[i] = uint128(_reward / SCALING_FACTOR) - _rewardDebt[i];
-//            if (vestingRatio > 0) {
-//                // calc which part should be payed immediately and with vesting from new reward
-//                uint128 vesting_part = (new_entitled[i] * vestingRatio) / MAX_VESTING_RATIO;
-//                uint128 clear_part = new_entitled[i] - vesting_part;
-//
-//                if (lastRewardTime < _farmEndTime || _farmEndTime == 0) {
-//                    newly_vested[i] = _computeVestedForNewlyEntitled(vesting_part, _poolLastRewardTime, _farmEndTime);
-//                } else {
-//                    // no new entitled rewards after farm end, nothing to compute
-//                    newly_vested[i] = 0;
-//                }
-//
-//                // now calculate newly vested part of old entitled reward
-//                uint32 age2 = _poolLastRewardTime >= vestingTime[i] ? vestingPeriod : _poolLastRewardTime - lastRewardTime;
-//                uint256 _vested = uint256(entitled[i]) * age2;
-//                uint128 to_vest = age2 >= vestingPeriod
-//                ? entitled[i]
-//                : uint128(_vested / (vestingTime[i] - lastRewardTime));
-//
-//                // amount of reward vested from now
-//                uint128 remainingEntitled = entitled[i] == 0 ? 0 : entitled[i] - to_vest;
-//                uint128 unreleasedNewly = vesting_part - newly_vested[i];
-//                uint128 pending = remainingEntitled + unreleasedNewly;
-//
-//                // Compute the vesting time (i.e. when the entitled reward to be all vested)
-//                if (pending == 0) {
-//                    new_vesting_time[i] = _poolLastRewardTime;
-//                } else if (remainingEntitled == 0) {
-//                    // only new reward, set vesting time to vesting period
-//                    new_vesting_time[i] = _poolLastRewardTime + vestingPeriod;
-//                } else if (unreleasedNewly == 0) {
-//                    // only unlocking old reward, dont change vesting time
-//                    new_vesting_time[i] = vestingTime[i];
-//                } else {
-//                    // "old" reward and, perhaps, "new" reward are pending - the weighted average applied
-//                    uint32 age3 = vestingTime[i] - _poolLastRewardTime;
-//                    uint32 period = uint32(((remainingEntitled * age3) + (unreleasedNewly * vestingPeriod)) / pending);
-//                    new_vesting_time[i] = _poolLastRewardTime + math.min(period, vestingPeriod);
-//                }
-//
-//                new_vesting_time[i] = _farmEndTime > 0 ? math.min(_farmEndTime + vestingPeriod, new_vesting_time[i]) : new_vesting_time[i];
-//                updated_entitled[i] = entitled[i] + vesting_part - to_vest - newly_vested[i];
-//                newly_vested[i] += to_vest + clear_part;
-//            } else {
-//                newly_vested[i] = new_entitled[i];
-//            }
-//        }
-//
-//        return (updated_entitled, newly_vested, new_vesting_time);
-//    }
+    // row sum
+    function _rangeSum(uint64 range) internal pure returns (uint64) {
+        return (range % 2) == 0 ? ((range / 2) * range + (range / 2)) : (((range / 2) + 1) * range);
+    }
 
+    // interval should be less than max
+    function _unlockedShareForInterval(uint32 interval, uint32 max) internal pure returns (uint256) {
+        return ((_rangeSum(uint64(interval)) * SCALING_FACTOR) / max) / interval;
+    }
+
+    // calculate how much will be unlocked after given time passed for given locked amount with vesting period
+    function _calcUnlockedForInterval(
+        uint128 _locked, uint32 _passed, uint32 _vestingPeriod
+    ) internal view returns (uint128 unlocked, uint128 remaining_locked) {
+        remaining_locked = _locked;
+
+        if (_passed > _vestingPeriod) {
+            uint32 periods_passed = (_passed / _vestingPeriod) - 1;
+            uint32 full_unlocked_part = periods_passed * _vestingPeriod + _passed % _vestingPeriod;
+
+            remaining_locked -= uint128(math.muldiv(math.muldiv(full_unlocked_part, SCALING_FACTOR, _passed), remaining_locked, SCALING_FACTOR));
+            _passed -= full_unlocked_part;
+        }
+
+        remaining_locked -= uint128(math.muldiv(remaining_locked, _unlockedShareForInterval(_passed, _vestingPeriod), SCALING_FACTOR));
+        return (_locked - remaining_locked, remaining_locked);
+    }
+
+    function _calcUnlocked(
+        uint128 _locked, uint32 _poolLastRewardTime, uint32 _lastRewardTime, uint32 _farmEndTime, uint32 _vestingPeriod
+    ) internal view returns (uint128 _unlocked) {
+        // some safety checks
+        // no new entitled rewards after farm end, nothing to compute
+        if (_locked == 0 || (_farmEndTime > 0 && _lastRewardTime >= _farmEndTime)) {
+            return 0;
+        }
+
+        _farmEndTime = _farmEndTime == 0 ? _poolLastRewardTime : _farmEndTime;
+        uint32 closestPoint = math.min(_farmEndTime, _poolLastRewardTime);
+
+        (uint128 _unlocked_before, uint128 _locked_before) = _calcUnlockedForInterval(_locked, closestPoint - _lastRewardTime, _vestingPeriod);
+        if (_poolLastRewardTime > _farmEndTime) {
+            _unlocked += _locked_before * math.min(_poolLastRewardTime - _farmEndTime, _vestingPeriod) / _vestingPeriod;
+        }
+        _unlocked += _unlocked_before;
+    }
+
+    function _computeVesting(
+        uint128 _balance,
+        uint128 _locked,
+        uint128 _rewardDebt,
+        uint128 _accRewardPerShare,
+        uint32 _poolLastRewardTime,
+        uint32 _lastRewardTime,
+        uint32 _farmEndTime,
+        uint32 _vestingPeriod,
+        uint32 _vestingRatio,
+        uint32 _vestingTime
+    ) internal view returns (uint128 updated_locked, uint128 new_unlocked, uint32 new_vesting_time) {
+        uint128 new_reward = math.muldiv(_balance, _accRewardPerShare, SCALING_FACTOR) - _rewardDebt;
+
+        if (_vestingRatio > 0) {
+            // calc which part should be payed immediately and with vesting from new reward
+            uint128 new_vesting = (new_reward * _vestingRatio) / MAX_VESTING_RATIO;
+            uint128 clear_part = new_reward - new_vesting;
+
+            new_unlocked = _calcUnlocked(new_vesting, _poolLastRewardTime, lastRewardTime, _farmEndTime, _vestingPeriod);
+
+            // now calculate newly unlocked part of old locked reward
+            uint32 passed = _poolLastRewardTime >= _vestingTime ? _vestingPeriod : _poolLastRewardTime - _lastRewardTime;
+            uint128 unlocked_old = passed >= _vestingPeriod
+            ? _locked
+            : uint128(math.muldiv(_locked, passed, _vestingTime - lastRewardTime));
+
+            // amount of reward locked from now
+            uint128 remaining_locked = _locked == 0 ? 0 : _locked - unlocked_old;
+            uint128 new_locked = new_vesting - new_unlocked;
+            uint128 pending = remaining_locked + new_locked;
+
+            // Compute the vesting time (i.e. when the locked reward to be all unlocked)
+            if (pending == 0) {
+                new_vesting_time = _poolLastRewardTime;
+            } else if (remaining_locked == 0) {
+                // only new reward, set vesting time to vesting period
+                new_vesting_time = _poolLastRewardTime + _vestingPeriod;
+            } else if (new_locked == 0) {
+                // only unlocking old reward, dont change vesting time
+                new_vesting_time = _vestingTime;
+            } else {
+                // "old" reward and, perhaps, "new" reward are pending - the weighted average applied
+                uint32 passed_2 = _vestingTime - _poolLastRewardTime;
+                uint32 period = uint32(((remaining_locked * passed_2) + (new_locked * _vestingPeriod)) / pending);
+                new_vesting_time = _poolLastRewardTime + math.min(period, _vestingPeriod);
+            }
+
+            new_vesting_time = _farmEndTime > 0 ? math.min(_farmEndTime + _vestingPeriod, new_vesting_time) : new_vesting_time;
+            updated_locked = _locked + new_vesting - unlocked_old - new_unlocked;
+            new_unlocked += unlocked_old + clear_part;
+        } else {
+            new_unlocked = new_reward;
+            updated_locked = _locked;
+            new_vesting_time = _vestingTime;
+        }
+    }
 }
